4a92115a6895f79d72c235bd3928ddf5be87d78d
Henri Susanto <henry.dinus@gmail.com>
2023-11-13 05:11:39 +0700

    login

---
+++ index.js
@@ -0,0 +1,73 @@
+/**
+ * Copyright 2017 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Search developers.google.com/web for articles tagged
+ * "Headless Chrome" and scrape results from the results page.
+ */
+
+'use strict';
+const puppeteer = require('puppeteer');
+(async () => {
+    try {
+        const browser = await puppeteer.launch({
+            defaultViewport: null,
+            ignoreHTTPSErrors: true,
+            slowMo: 100,
+            headless: false,
+            // devtools: true
+        })
+
+        const [page] = await browser.pages()
+        const sessionId = `67d605fd9bbf315494f786f9f5bc2581`
+
+        await page.setCookie({
+            name: `sessionid`,
+            value: sessionId,
+            domain: '.tiktok.com',
+            path: '/',
+            expires: -1,
+            size: sessionId.length,
+            httpOnly: false,
+            secure: true,
+            session: false,
+            sameSite: 'None',
+            sameParty: false,
+            sourceScheme: 'Secure',
+            sourcePort: 443
+        })
+        await page.goto(`https://tiktok.com/`)
+        // await browser.close()
+    } catch (e) {
+        console.error(e)
+    }
+})()
+
+const click = async function (page, selector) {
+    await page.waitForSelector(selector)
+    await page.hover(selector)
+    await page.$eval(selector, (btn) => btn.click())
+}
+
+const type = async function (page, selector, letters) {
+    await page.waitForSelector(selector);
+    await page.hover(selector);
+    await page.focus(selector);
+    await page.keyboard.type(letters);
+}
+
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+const random = function (min, max) { return Math.floor(Math.random() * (max - min + 1) + min) }  
